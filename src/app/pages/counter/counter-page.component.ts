f8af731a9f77d9b8efacde40082400c34259b1a4
Tío Gallo <juanftp100@gmail.com>
2025-11-27 13:31:10 -0300

    uso de señales

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
+// , changeDetection: ChangeDetectionStrategy.OnPush,
 @Component({
   templateUrl: './counter-page.component.html',
   styles: `
@@ -12,16 +13,28 @@ import { Component } from "@angular/core";
 })
 export class CounterPageComponent {
   counter = 10;
+  counterSignals = signal(100);
+
+  // para comprobar que el zonejs no funciona en el setinterval
+  // constructor() {
+  //   setInterval(() => {
+  //     this.counter += 1;
+  //     this.counterSignals.update(elemento => elemento + 1);
+  //   }, 2000);
+  // }
 
   increaseBy() {
     this.counter += 1;
+    this.counterSignals.update(valor => valor + 1);
   }
 
   decreaseBy() {
     this.counter -= 1;
+    this.counterSignals.update(valor => valor - 1);
   }
 
   resetCounter() {
     this.counter = 10;
+    this.counterSignals.set(100);
   }
 }
