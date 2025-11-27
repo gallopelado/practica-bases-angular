f2b2483ea2c3aedc134c8eb5a7c852e323572707
Tío Gallo <juanftp100@gmail.com>
2025-11-27 17:00:40 -0300

    uso del for

--- src/app/pages/dragonball-page/dragonball-page.component.ts
+++ src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball-page',
@@ -6,5 +12,20 @@ import { Component } from '@angular/core';
   templateUrl: './dragonball-page.component.html'
 })
 export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: "Goku", power: 9001 },
+    { id: 2, name: "Vegeta", power: 8500 },
+    { id: 3, name: "Piccolo", power: 7500 }
+  ]);
+
+  showColorLevel(power: number): string {
+    if (power > 9000) {
+      return "success";
+    } else if (power > 8000) {
+      return "warning";
+    } else {
+      return "secondary";
+    }
+  }
 
 }
