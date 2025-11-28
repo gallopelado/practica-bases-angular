f017b0cba4c01d0a17c5c0ba77be12679f5da9c2
Tío Gallo <juanftp100@gmail.com>
2025-11-28 09:29:40 -0300

    cambio de listas a cards, se añadieron imágenes según enlace.

--- src/app/pages/dragonball-page/dragonball-page.component.ts
+++ src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -4,18 +4,20 @@ interface Character {
   id: number;
   name: string;
   power: number;
+  urlImage?: string;
 }
 
 @Component({
   selector: 'app-dragonball-page',
   imports: [],
-  templateUrl: './dragonball-page.component.html'
+  templateUrl: './dragonball-page.component.html',
+  styleUrl: './dragonball-page.component.css'
 })
 export class DragonballPageComponent {
   characters = signal<Character[]>([
-    { id: 1, name: "Goku", power: 9001 },
-    { id: 2, name: "Vegeta", power: 8500 },
-    { id: 3, name: "Piccolo", power: 7500 }
+    { id: 1, name: "Goku", power: 9001, urlImage: 'https://dragonball-api.com/characters/goku_normal.webp' },
+    { id: 2, name: "Vegeta", power: 8500, urlImage: 'https://dragonball-api.com/characters/vegeta_normal.webp' },
+    { id: 3, name: "Piccolo", power: 7500, urlImage: 'https://dragonball-api.com/characters/picolo_normal.webp' }
   ]);
 
   showColorLevel(power: number): string {
