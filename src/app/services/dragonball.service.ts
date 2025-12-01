b37676fed97515ba223676671f8abb62ee81028e
Tío Gallo <juanftp100@gmail.com>
2025-12-01 15:32:12 -0300

    feat: recuperar valores del storage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,13 +1,19 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+const loadFromStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+}
+
 @Injectable({providedIn: 'root'})
 export class DragonballService {
   name = signal<string>("");
   power = signal<number>(0)
-  characters = signal<Character[]>([
-    { id: 1, name: "Goku", power: 9001, urlImage: 'https://dragonball-api.com/characters/goku_normal.webp' }
-  ]);
+  characters = signal<Character[]>(loadFromStorage());
+  // characters = signal<Character[]>([
+  //   { id: 1, name: "Goku", power: 9001, urlImage: 'https://dragonball-api.com/characters/goku_normal.webp' }
+  // ]);
 
   saveToLocalStorage = effect(() => {
     console.log(`Contados de elementos characters ${this.characters().length}`);
